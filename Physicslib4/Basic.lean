3b5c14b8e271adc2957d894eb18213cad8093b5f
KellyJDavis <kdavis@alum.mit.edu>
2026-06-04 09:54:46 +0200

    Added doc strings in std format

--- Physicslib4/Basic.lean
+++ Physicslib4/Basic.lean
@@ -1,1 +1,23 @@
+/-
+Copyright (c) 2026 Lean Community. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Lean Community
+-/
+import Mathlib.Data.String.Basic
+
+/-!
+# Hello World String
+
+This file defines a basic greetings string constant.
+
+## Main definitions
+
+* `hello`: A constant string containing the text "world".
+
+## Tags
+
+greeting, string, tutorial
+-/
+
+/-- A constant string greeting the world. -/
 def hello := "world"
