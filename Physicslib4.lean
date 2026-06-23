a68e9a8efb2b02bd4e028bf5502b8ff7fa6dfe0c
KellyJDavis <kdavis@alum.mit.edu>
2026-06-23 09:46:20 +0200

    Ran lake exe mk_all

--- Physicslib4.lean
+++ Physicslib4.lean
@@ -9,7 +9,6 @@ import Physicslib4.AQFT.HaagKastler.QuasilocalAction
 import Physicslib4.AQFT.HaagKastler.QuasilocalAlgebra
 import Physicslib4.AQFT.HaagKastler.QuasilocalCompleteness
 import Physicslib4.AQFT.HaagKastler.QuasilocalIntertwiner
-import Physicslib4.AQFT.KMS
 import Physicslib4.AQFT.HaagKastlerCurved.Concrete
 import Physicslib4.AQFT.HaagKastlerCurved.CovariantState
 import Physicslib4.AQFT.HaagKastlerCurved.EinsteinCausality
@@ -23,6 +22,7 @@ import Physicslib4.AQFT.HaagKastlerCurved.Net
 import Physicslib4.AQFT.HaagKastlerCurved.Spacetime
 import Physicslib4.AQFT.HaagKastlerCurved.StabilizerAction
 import Physicslib4.AQFT.HaagKastlerCurved.StabilizerKMS
+import Physicslib4.AQFT.KMS
 import Physicslib4.Analysis.CStarDenseExtend
 import Physicslib4.Analysis.HorizontalLineRemovable
 import Physicslib4.Analysis.StripPeriodicExtension
