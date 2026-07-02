dfe251ffd666e13fd45f27e4a1a45083be50f8f7
KellyJDavis <kdavis@alum.mit.edu>
2026-07-02 10:37:06 +0200

    Ran "lake exe mk_all"

--- Physicslib4.lean
+++ Physicslib4.lean
@@ -15,9 +15,9 @@ import Physicslib4.AQFT.HaagKastler.QuasilocalIntertwiner
 import Physicslib4.AQFT.HaagKastler.QuasilocalKMS
 import Physicslib4.AQFT.HaagKastler.VacuumState
 import Physicslib4.AQFT.HaagKastlerCurved.Concrete
-import Physicslib4.AQFT.HaagKastlerCurved.GeometricCovariance
 import Physicslib4.AQFT.HaagKastlerCurved.CovariantState
 import Physicslib4.AQFT.HaagKastlerCurved.EinsteinCausality
+import Physicslib4.AQFT.HaagKastlerCurved.GeometricCovariance
 import Physicslib4.AQFT.HaagKastlerCurved.IdentityComponent
 import Physicslib4.AQFT.HaagKastlerCurved.IsometricCovariance
 import Physicslib4.AQFT.HaagKastlerCurved.Isotony
